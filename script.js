bad421c8e7b77d927c5c129464d4adcb0ae8bff4
Thomas Martin <tomm2822@gmail.com>
2024-02-27 23:58:35 -0500

    Adds Resume Section. Fixes Typos

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "QA Ninja | Power Home Remodeling";
 
-const p = document.createElement("p");
-p.textContent = "Currently a Qualty Assurance tester for the BT Department at Power Homde Remodeling. I have a BS in Computer Science from West Chester University. I had a concentration in Java and Python. I user to work as a Wordpress Developer before starting my Power journey."
+const h3 = document.createElement("h3");
+h3.textContent = "Currently a Qualty Assurance tester for the BT Department at Power Home Remodeling. I have a BS in Computer Science from West Chester University. I had a concentration in Java and Python. I used to work as a Wordpress Developer before starting my Power journey."
 
 
 document.getElementById("about").appendChild(h2);
 
-document.getElementById("about").appendChild(p);
+document.getElementById("about").appendChild(h3);
 
 function responsive() {
     var x = document.getElementById("myTopnav");
