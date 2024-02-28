c1ab3e8a5d9e5796dfe90a1f40c1cf29d60cabb2
Thomas Martin <tomm2822@gmail.com>
2024-02-28 00:38:24 -0500

    Updates backgrounds and fonts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "QA Ninja | Power Home Remodeling";
+h2.textContent = "Quality Assurance Tester";
 
 const h3 = document.createElement("h3");
 h3.textContent = "Currently a Qualty Assurance tester for the BT Department at Power Home Remodeling. I have a BS in Computer Science from West Chester University. I had a concentration in Java and Python. I used to work as a Wordpress Developer before starting my Power journey."
